7b17dfcbe97d03a0c88e8df710d8ce1bfa96f23a
Ross Berkland <ross@made.co.za>
2016-10-06 21:26:31 +0200

    Add basic operations & test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # node-server-calc
-Calculator as a service
+> Calculator as a service ➗✖️➕➖
+
+---
+## Install
+1. `git clone https://github.com/madeagency/node-server-calc.git`
+2. `npm install`
+
+---
+## Start (development)
+`npm start`
+
+---
+## Start (production)
+`npm run build; npm run serve`
+
+---
+## Test
+`npm test`
+
+---
+## Requirementss
+- Written in `Babel`
+- Uses `express` and `body-parser`
--- test/index.js
+++ test/index.js
@@ -1,13 +1,31 @@
-import http from 'http';
+import fetch from 'node-fetch';
 import assert from 'assert';
 
 import '../lib/index.js';
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
-      assert.equal(200, res.statusCode);
-      done();
-    });
-  });
+async function assertGet() {
+  let response = await fetch('http://localhost:3000/multiply/7/7')
+  let result = await response.json()
+  assert.equal(49, result)
+}
+
+async function assertPost() {
+  let response = await fetch('http://localhost:3000/', {
+    method: 'POST',
+    body: JSON.stringify({
+      operation: '/',
+      number1: 81,
+      number2: 12
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  let result = await response.json()
+  assert.equal(6.75, result)
+}
+
+describe('Node Server Calculator', () => {
+  it('should return 49', assertGet)
+  it('should return 6.75', assertPost)
 });
